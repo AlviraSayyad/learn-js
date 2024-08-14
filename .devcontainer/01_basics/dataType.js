eb65638ca8bd77287ca9e9afe0d94594ea66b66c
AlviraSayyad <152610818+AlviraSayyad@users.noreply.github.com>
2024-08-14 07:20:23 +0000

    new file addes

---
+++ .devcontainer/01_basics/dataType.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) --> we are using node js not browser
+
+let name="alvira"  //string
+let age=22    //int(number => 2 to power 53)
+let isLogged=true  //boolean
+let a=null    // standalone value 
+let b;   //undefined 
+
+// to check the type
+console.log(typeof(b)); //object
+console.log(typeof(a)); //undefined
---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId=111
+let accountEmail="alvira@gmail.com"
+var accountPassword="00000"
+accoutCity="Mahagaon"
+let accountState ;
+
+//accountId=222
+accountEmail="alvira27@gmail.com"
+accountPassword="12345"
+accoutCity="gadhinglaj"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accoutCity,accountState])
+
+/*
+   prefer not to use var bcz of block scope and functional scope issue
+*/
