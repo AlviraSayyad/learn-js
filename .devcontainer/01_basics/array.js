d54f6c0d756c443a0606aef57d8ed1a149a70a48
AlviraSayyad <152610818+AlviraSayyad@users.noreply.github.com>
2024-08-21 07:37:02 +0000

    array

---
+++ .devcontainer/01_basics/array.js
@@ -0,0 +1,28 @@
+const myArr=[0,1,2,3,4,5]
+
+//console.log(myArr[1])
+
+//Array methods
+
+/*
+myArr.push(6)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+
+
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(3))
+
+const newArr=myArr.join()
+console.log(newArr)
+*/
+
+const myn=myArr.slice(1,3);
+console.log("A",myArr)
+console.log(myn)
+
+const myn1=myArr.splice(1,3);
+console.log("A",myArr)
+console.log(myn1)
